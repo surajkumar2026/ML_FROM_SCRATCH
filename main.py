d8233f6c46bd8277fcaf2daf51671de7b4e19652
Surajkumar9798 <me22b1073@iiitdm.ac.in>
2025-03-28 21:14:30 +0530

    Linear Regression

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ if __name__=="__main__":
     X=[[1,2,3],[4,5,6]]
     x_array=np.array(X)
     Y=[4,5]
-    test_X=[[5,7,8],[9,8,7]]
+    test_X=[[5,7,8],[9,8,1]]
+
 
     linerregression= Excutation(1000,3,0.001,X,Y,test_X)
     linerregression.modeul(x_array,Y,test_X)
